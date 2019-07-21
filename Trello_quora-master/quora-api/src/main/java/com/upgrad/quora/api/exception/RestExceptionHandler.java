a902491a4fc80532ddf90a4a5fe777ad1d085208
Nimisha <32481399+linientlearner@users.noreply.github.com>
2019-07-21 14:20:02 +0530

    Update RestExceptionHandler.java

--- Trello_quora-master/quora-api/src/main/java/com/upgrad/quora/api/exception/RestExceptionHandler.java
+++ Trello_quora-master/quora-api/src/main/java/com/upgrad/quora/api/exception/RestExceptionHandler.java
@@ -24,7 +24,7 @@ public class RestExceptionHandler {
         return new ResponseEntity<ErrorResponse>(new ErrorResponse()
                 .code(exc.getCode())
                 .message(exc.getErrorMessage()),
-                HttpStatus.UNAUTHORIZED);
+                HttpStatus.FORBIDDEN);
     }
 
     @ExceptionHandler(SignOutRestrictedException.class)
